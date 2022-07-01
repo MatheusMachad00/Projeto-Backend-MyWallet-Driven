dc8bf6d450a8976a9292b544a451ae8c6f50a1de
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-07-01 14:13:12 -0300

    Feat: Function to post a debit created

--- src/controllers/activityController.js
+++ src/controllers/activityController.js
@@ -1,12 +1,11 @@
 import db from '../database/db.js';
 
-export async function activityStatement (req, res) {
+export async function activityStatement(req, res) {
   try {
     const userData = res.locals.userData;
-    
-    const user = await db.collection("users").findOne({_id: userData._id })
-    /* const activity = await db.collection("users").findOne({ activity }) */
-    /* console.log(activity) */
+
+    const user = await db.collection("users").findOne({ _id: userData._id })
+
     res.send(user)
   } catch (error) {
     res.sendStatus(500);
@@ -14,16 +13,16 @@ export async function activityStatement (req, res) {
   }
 };
 
-export async function activityIn (req, res) {
+export async function activityIn(req, res) {
   try {
     const inOutData = res.locals.inOutData;
     const userData = res.locals.userData;
 
     await db.collection('users').updateOne({
       _id: userData._id
-    }, { $push: { activity: [{...inOutData, type: 'in'}] } });
+    }, { $push: { activity: [{ ...inOutData, type: 'in' }] } });
 
-    res.status(201).send({message: "tudo ok"});
+    res.status(201).send({ message: "tudo ok" });
 
   } catch (error) {
     res.sendStatus(500);
@@ -31,10 +30,16 @@ export async function activityIn (req, res) {
   }
 };
 
-export async function activityOut (req, res) {
+export async function activityOut(req, res) {
   try {
-    /* await mongoClient.connect();
-    const db = mongoClient.db(process.env.DB_NAME); */
+    const inOutData = res.locals.inOutData;
+    const userData = res.locals.userData;
+
+    await db.collection('users').updateOne({
+      _id: userData._id
+    }, { $push: { activity: [{ ...inOutData, type: 'out' }] } });
+
+    res.status(201).send({ message: "tudo ok" });
 
   } catch (error) {
     res.sendStatus(500);
